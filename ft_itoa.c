fe27600f6b85203c419878bb02ff2addd3dfd5a3
Marta <martagarzia3@gmail.com>
2024-12-18 16:03:11 +0100

    added functions to libft.h

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: marta <marta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:04:07 by marta             #+#    #+#             */
-/*   Updated: 2024/12/18 12:50:54 by marta            ###   ########.fr       */
+/*   Updated: 2024/12/18 15:59:57 by marta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static unsigned int	ft_number_size(int number)
+{
+	unsigned int	length;
+
+	length = 0;
+	if (number == 0)
+		return (1);
+	if (number < 0)
+		length++;
+	while (number != 0)
+	{
+		number /= 10;
+		length++;
+	}
+	return (length);
+}
+
+static char	*ft_convert_to_string(int n, unsigned int length)
+{
+	char			*string;
+	unsigned int	number;
+
+	string = (char *)malloc(sizeof(char) * (length + 1));
+	if (!string)
+		return (NULL);
+	string[length] = '\0';
+	if (n < 0)
+	{
+		string[0] = '-';
+		number = -n;
+	}
+	else
+		number = n;
+	while (number > 0)
+	{
+		string[--length] = (number % 10) + '0';
+		number /= 10;
+	}
+	return (string);
+}
+
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		len;
-	long	nb;
+	char			*string;
+	unsigned int	length;
 
-	nb = n;
-	len = 1;
-	while (n && len++)
-		n /= 10;
-	str = malloc(len + (nb < 0) + 1);
-	if (!str)
-		return (NULL);
-	str[len + (nb < 0)] = '\0';
-	if (nb < 0)
-		nb = -nb;
-	while (len--)
+	if (n == 0)
 	{
-		str[len + (nb < 0)] = '0' + (nb % 10);
-		nb /= 10;
+		string = (char *)malloc(sizeof(char) * 2);
+		if (!string)
+			return (NULL);
+		string[0] = '0';
+		string[1] = '\0';
+		return (string);
 	}
-	if (str[0] == '\0')
-		str[0] = '-';
-	return (str);
+	length = ft_number_size(n);
+	string = ft_convert_to_string(n, length);
+	return (string);
 }
 /*
 int	main(void)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: marta <marta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:37:42 by marta             #+#    #+#             */
-/*   Updated: 2024/12/18 15:40:13 by marta            ###   ########.fr       */
+/*   Updated: 2024/12/18 16:02:22 by marta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+int		ft_atoi(const char *str);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
