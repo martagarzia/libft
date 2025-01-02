fe23be25ca58c2e9a9b12d13643db731c648ca34
Marta <martagarzia3@gmail.com>
2025-01-02 13:39:01 +0100

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: marta <marta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:37:42 by marta             #+#    #+#             */
-/*   Updated: 2024/12/30 13:40:44 by marta            ###   ########.fr       */
+/*   Updated: 2025/01/02 13:38:46 by marta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int		ft_isalnum(int c);
 int		ft_isascii(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *ptr, int x, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *d, const void *s, size_t n);
 
 int		ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
