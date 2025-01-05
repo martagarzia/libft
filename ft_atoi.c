6b6501f2ff7cd5a70de01d696c0001ccdb9f313c
Marta <martagarzia3@gmail.com>
2025-01-06 00:49:41 +0100

    libft, atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,60 @@
 /*   By: mgarzia <mgarzia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 18:17:14 by mgarzia           #+#    #+#             */
-/*   Updated: 2025/01/02 13:40:50 by mgarzia          ###   ########.fr       */
+/*   Updated: 2025/01/06 00:49:02 by mgarzia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+atoi = ASCII to integer
+
+converte una stringa di caratteri che rappresenta un numero intero 
+in un valore numerico di tipo int.
+
+- ignora gli spazi bianchi iniziali (spazi, tab, ecc.).
+- interpreta + e - prima del numero
+- coverte '0' - '9' in intero
+
+- si ferma al primo carattere non numerico
+
+ritorna
+- int, convertito dai caratteri che rappresentano l'intero
+- se non ci sono caratteri numerici = comportamento non definito
+
+9 - 13 = caratteri di controllo \t \n \v \f \r
+
+*s è un puntatore a una stringa, 
+che parte sempre dal primo posto nella stringa,
+quindi possiamo usarlo per calcolare il movimento nella stringa usando s++,
+senza dover usare i.
+
+per result usiamo long e non int 
+perchè permettere di utiliazzare numeri più grandi,
+evitando overflow.
+*/
 
 #include "libft.h"
 #include <stdio.h>
 
-int	ft_atoi(const char *str)
+int	ft_atoi(const char *s)
 {
 	long	result;
 	int		sign;
 
-	sign = 1;
 	result = 0;
-	while ((*str >= 9 && *str <= 13) || *str == ' ')
-		str++;
-	if (*str == '-' || *str == '+')
+	sign = 1;
+	while ((*s >= 9 && *s <= 13) || *s == ' ')
+		s++;
+	if (*s == '-' || *s == '+')
 	{
-		if (*str == '-')
+		if (*s == '-')
 			sign = -1;
-		str++;
+		s++;
 	}
-	while (*str >= '0' && *str <= '9')
+	while (*s >= '0' && *s <= '9')
 	{
-		result = result * 10 + (*str - '0');
-		str++;
+		result = result * 10 + (*s - '0');
+		s++;
 	}
 	return ((int)(result * sign));
 }
@@ -40,6 +68,5 @@ int	main(void)
 {
 	printf("%d\n", ft_atoi("   -42"));
 	printf("%d\n", ft_atoi("12345abc"));
-	return (0);
 }
 */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mgarzia <mgarzia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:45:01 by mgarzia           #+#    #+#             */
-/*   Updated: 2025/01/06 00:10:00 by mgarzia          ###   ########.fr       */
+/*   Updated: 2025/01/06 00:49:10 by mgarzia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,6 @@ char	*ft_strdup(const char *s1);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-
 
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
